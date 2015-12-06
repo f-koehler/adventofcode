1596716e1407f397a97435fce1b7ca0f749b39d1
Fabian Köhler <fkoehler1024@googlemail.com>
2015-12-06 18:49:31 +0100

    add d1 part 2

--- d1.py
+++ d1.py
@@ -3,9 +3,16 @@ if __name__ == "__main__":
     with open("d1.txt") as f:
         innstruction = "".join(f.readlines())
         floor = 0
+        i = 0
+        entered_basement = False
         for c in innstruction:
             if c == '(':
                 floor += 1
             else:
                 floor -= 1
+                if not entered_basement and floor == -1:
+                    entering_position = i
+                    entered_basement = True
+            i += 1
         print(floor)
+        print(entering_position+1)
