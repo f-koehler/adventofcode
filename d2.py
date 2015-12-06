f741edd107d85d62d3aadc390bdcb0da55b6df50
Fabian Köhler <fkoehler1024@googlemail.com>
2015-12-06 18:55:06 +0100

    add d2 part 2

--- d2.py
+++ d2.py
@@ -5,6 +5,14 @@ def gift_area(l, w, h):
     side_c = l*h
     return 2*side_a+2*side_b+2*side_c+min((side_a, side_b, side_c))
 
+def gift_ribbon(l, w, h):
+    side_a = 2*l+2*w
+    side_b = 2*w+2*h
+    side_c = 2*l+2*h
+    ribbon = min((side_a, side_b, side_c))
+    ribbon += l*w*h
+    return ribbon
+
 if __name__ == "__main__":
     with open("d2.txt") as f:
         lines = f.readlines()
@@ -13,6 +21,9 @@ if __name__ == "__main__":
         fields = l.split("x")
         gifts.append([int(f) for f in fields])
     area = 0
+    ribbon = 0
     for g in gifts:
         area += gift_area(g[0], g[1], g[2])
+        ribbon += gift_ribbon(g[0], g[1], g[2])
     print(area)
+    print(ribbon)
